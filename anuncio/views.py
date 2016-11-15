71bb60064dd42a93f35075bc10427ce677fc3536
Igor Abreu <igor.abreu.da.silva@cern.ch>
2016-11-15 20:54:46 -0300

    Arquivos gerados automaticamente pelo Django

--- anuncio/views.py
+++ anuncio/views.py
@@ -10,5 +10,4 @@ def anuncio(request):
         anuncio.data_inicio = anuncio.data_inicio.strftime('%d/%m/%Y %H:%M')
         anuncio.data_fim = anuncio.data_fim.strftime('%d/%m/%Y %H:%M')
         anuncio.data_criacao = anuncio.data_criacao.strftime('%d/%m/%Y %H:%M')
-        print anuncio
     return render(request, 'anuncios/anuncios.html', {'anuncios': anuncios})
