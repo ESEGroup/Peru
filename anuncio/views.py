d0b2eef907495c7df825dff7a850887014832261
Igor Abreu <igor.abreu.da.silva@cern.ch>
2016-11-17 10:58:50 -0300

    Remover loop com formatacao de datas

--- anuncio/views.py
+++ anuncio/views.py
@@ -6,8 +6,4 @@ from datetime import datetime
 # Create your views here.
 def anuncio(request):
     anuncios = Anuncio.objects.all()
-    for anuncio in anuncios:
-        anuncio.data_inicio = anuncio.data_inicio.strftime('%d/%m/%Y %H:%M')
-        anuncio.data_fim = anuncio.data_fim.strftime('%d/%m/%Y %H:%M')
-        anuncio.data_criacao = anuncio.data_criacao.strftime('%d/%m/%Y %H:%M')
     return render(request, 'anuncios/anuncios.html', {'anuncios': anuncios})
