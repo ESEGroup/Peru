aa92135bacfd1566c7543410b6c31eea46d12597
Igor Abreu <igor.abreu.da.silva@cern.ch>
2016-12-17 18:09:20 -0300

    Resolver problema da Time zone para insercao de dados

--- PainelDigital/settings.py
+++ PainelDigital/settings.py
@@ -106,17 +106,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Sao_Paulo'
-
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
